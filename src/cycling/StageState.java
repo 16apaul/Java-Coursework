f44bb4459d6f439d0fdb0058f2e04af59085b20d
16apaul <86714317+16apaul@users.noreply.github.com>
2024-03-04 23:17:56 +0000

    Add files via upload

---
+++ src/cycling/StageState.java
@@ -0,0 +1,25 @@
+package cycling;
+
+    /**
+ * This enum is used to represent the stage states  on road races.
+ * 
+ * @author Aritra
+ * @version 1.0
+ *
+ */
+public enum StageState {
+	
+	/**
+	 * Used when stage is created
+	 */
+	ADDING_CHECKPOINTS,
+	
+	/**
+	 * When stage is concluded
+	 */
+	WAITING_FOR_RESULTS,
+	
+	
+}
+
+
